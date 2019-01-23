cb53b3abe6582002d91a8c2ed17fdd2184083bcd
scotidodson <scotidodson@gmail.com>
2019-01-22 22:58:46 -0500

    added logo and created skeleton

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navigation = document.querySelector('.navigation')
+  const logo = document.querySelector('.sd-logo')
 
   // ------------------------- LISTENERS ------------------------------------
 
     navigation.addEventListener('click', (event) => {
       // check for value & change page accordingly
+      console.log('clicked nav');
+    })
+
+    logo.addEventListener('mouseover', (event) => {
+      console.log('over logo');
+      const altLogo = Math.floor(Math.random() * 7)
+      logo.src=`./images/sd${altLogo}.png`
     })
 
 
